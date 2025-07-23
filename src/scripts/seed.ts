5bf1abab268975dc965aa7168340be88e6f90aa6
Abhishek Gupta <abhishek.gupta1@abhishekguptavsr-35.successivetech.com>
2025-07-23 16:13:35 +0530

    feat : assignment8

---
+++ src/scripts/seed.ts
@@ -0,0 +1,19 @@
+import { connectDB } from "../config/db";
+import Country from "../models/country.model";
+
+const countries = [
+  { name: "India", code: "IN" },
+  { name: "Australia", code: "AU" },
+  { name: "England", code: "ENG" },
+  { name: "South Africa", code: "SA" },
+];
+
+const seedCountries = async () => {
+  await connectDB();
+  await Country.deleteMany({});
+  await Country.insertMany(countries);
+  console.log("Countries seeded");
+  process.exit(0);
+};
+
+seedCountries();
