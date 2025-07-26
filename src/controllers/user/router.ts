7290e8107c760077ff0e3e267f9c4eb561d69d18
Abhishek Gupta <abhishek.gupta1@abhishekguptavsr-35.successivetech.com>
2025-07-26 15:07:37 +0530

    fix : userrouter

--- src/controllers/user/router.ts
+++ src/controllers/user/router.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 import { login, createUser,getUserProfile } from './userController';
 import { authenticateToken, generateToken } from '../../middlewares/auth';
+import { validateSchema } from "../../middlewares/validateSchema";
+import { validateQuery } from "../../middlewares/validateQuery";
+import { validateLocation } from "../../middlewares/validateLocation";
+import { dynamicValidation } from "../../middlewares/dynamicValidation";
 
 
 const router = express.Router();
@@ -8,7 +12,11 @@ const router = express.Router();
 
 router.post('/login', login)
 router.post('/register',createUser)
-router.get('/profile' ,authenticateToken,getUserProfile)
+router.get('/profile' ,authenticateToken,getUserProfile);
+router.post("/usercredentials",validateSchema);
+router.get("/fetchid/:id",validateQuery);
+router.get("/location",validateLocation);
+router.post('/user/register',dynamicValidation)
 
 
 
