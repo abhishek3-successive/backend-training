23995bb9e183075aa5434eb62eae6e5335ba7267
Abhishek Gupta <abhishek.gupta1@abhishekguptavsr-35.successivetech.com>
2025-07-30 17:31:50 +0530

    fix : router

--- src/router.ts
+++ src/router.ts
@@ -28,58 +28,6 @@ router.post('/seed', (req: Request , res: Response) =>{
     })
 })
 
-router.post('/bad-request' , (req : Request , res : Response )=>{
-    console.log("Incoming request body:", req.body);
-  const {name , email} = req.body;
-
-  if(!name || !email){
-    return res.status(400).json({
-        error : "Name and email are required",
-        status : 400
-    })
-  }
-
-  res.status(200).json({
-    error : "User created sucessfully",
-    data :{ name , email}
-  })
-});
-
-router.get('/unauthorized' , (req : Request , res : Response)=>{
-const token = req.headers.authorization;
-
-if(!token){
-    return res.status(401).json({
-        error : "please login first",
-        status : 401
-    });
-}
- res.status(200).json({
-        message : "Welcome! You are logged in"
-    })
-})
-
-router.get('/forbidden', (req: Request , res : Response)=>{
-    const userRole = req.headers['user-role'] as string;
-
-    if(userRole != 'admin'){
-        return res.status(403).json({
-            error : "you need admin acesses",
-            status : 403
-    })
-    }
-
-    res.status(200).json({
-        message : 'Welcome admin'
-})
-})
-
-router.get('/server-error', (req: Request , res : Response)=>{
-    res.status(500).json({
-        error : "Database Connection failed",
-        status : 500
-    })
-})
 
 
 router.use('/', userRouter);
