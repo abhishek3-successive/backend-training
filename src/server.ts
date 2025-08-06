7750a241d4e4ca4ab2cde7f279e83598432597fd
Abhishek Gupta <abhishek.gupta1@abhishekguptavsr-35.successivetech.com>
2025-08-06 16:16:53 +0530

    fix : changes

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const PORT = 5003;
 connectDB();
 
 const server = app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
