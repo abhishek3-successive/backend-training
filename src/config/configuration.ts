1a54ad1c93f85f2ab712fa46c89c8b2bee25ecf8
Abhishek Gupta <abhishek.gupta1@abhishekguptavsr-35.successivetech.com>
2025-08-07 18:20:01 +0530

    fix : configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,6 +5,5 @@ dotenv.config();
 const configurations = Object.freeze({
   mongo_uri: process.env.MONGO_DB_URI
 });
-console.log('MONGO_URI from .env:', process.env.MONGO_DB_URI); 
 
 export default configurations;
